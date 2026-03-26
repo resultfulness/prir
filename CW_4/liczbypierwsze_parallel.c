91efb4a4bf13b7e493861ec51ba6941c0f5e3444
resultfulness <alicja.rrosiak@gmail.com>
2026-03-26 13:32:57 +0100

    at least it's working

--- CW_4/liczbypierwsze_parallel.c
+++ CW_4/liczbypierwsze_parallel.c
@@ -7,17 +7,20 @@
 #define S (int)sqrt(N)
 #define M N/10
 
+int foo(const void *a, const void *b) {
+    return (*(int*) a - *(int *)b);
+}
+
 int main(int argc, char **argv) {
     long int tmp[S + 1]; /*tablica pomocnicza*/
     long int pierwsze[M]; /*liczby pierwsze w przedziale 2..N*/
-    long i, j, liczba, reszta;
+    long i, j, liczba;
     long int liczba_podzielników; /* l. podzielnikow*/
     long int liczba_liczb_pierwszych = 0; /*l. liczb pierwszych w tablicy pierwsze*/
     double czas; /*zmienna do  mierzenia czasu*/
     FILE *fp;
 
     /*wyznaczanie podzielnikow z przedzialow 2..S*/
-    #pragma omp parallel for private(i)
     for (i = 2; i <= S; i++) {
         tmp[i] = 1; /*inicjowanie*/
     }
@@ -27,7 +30,6 @@ int main(int argc, char **argv) {
             pierwsze[liczba_liczb_pierwszych++] = i; /*zapamietanie podzielnika*/
 
             /*wykreslanie liczb zlozonych bedacych wielokrotnosciami i*/
-            #pragma omp parallel for private(j)
             for (j = i + i; j <= S; j += i) {
                 tmp[j] = 0;
             }
@@ -36,23 +38,24 @@ int main(int argc, char **argv) {
 
     liczba_podzielników = liczba_liczb_pierwszych; /*zapamietanie liczby podzielnikow*/
 
-    int is_prime;
+    #pragma omp parallel for private(i)
     for (liczba = S + 1; liczba <= N; liczba++) {
-        is_prime = 0;
-
-        #pragma omp parallel for private(i, reszta)
         for (i = 0; i < liczba_podzielników; i++) {
-            reszta = (liczba % pierwsze[i]);
-            if (reszta == 0) { /* liczba zlozona */
-                is_prime = 1;
+            if (liczba % pierwsze[i] == 0) { /* liczba zlozona */
+                break;
             }
         }
 
-        if (is_prime) {
-            pierwsze[liczba_liczb_pierwszych] = liczba; /*zapamietanie liczby pierwszej*/
-            liczba_liczb_pierwszych += 1;
+        if (i == liczba_podzielników) {
+            #pragma omp critical
+            {
+                pierwsze[liczba_liczb_pierwszych] = liczba; /*zapamietanie liczby pierwszej*/
+                liczba_liczb_pierwszych += 1;
+            }
         }
     }
+
+    qsort(pierwsze, liczba_liczb_pierwszych, sizeof(long int), foo);
 
     if ((fp = fopen("primes.txt", "w")) == NULL) {
         printf("Nie moge otworzyc pliku do zapisu\n");
